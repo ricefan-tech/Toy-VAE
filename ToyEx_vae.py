2b471ddf1f356ba88be04abd883d70bf28e42ed4
ricefan-tech <76484583+ricefan-tech@users.noreply.github.com>
2020-12-22 15:43:13 +0100

    Update ToyEx_vae.py

--- ToyEx_vae.py
+++ ToyEx_vae.py
@@ -12,11 +12,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pdb
-from vae_tf1 import CVAE
+from vae_tf1 import VAE
 import mgarch as mg
 import statsmodels.api as sm
 from sklearn.preprocessing import MinMaxScaler
 
+#generate 3-dimensional samples with desired correlation and marginal distribution
 def getdata(dof=5, samp=10000, mean=[0.,2.,0.], final_cov=[1.,2.,1.], cov=[[1.,-0.8,0.],
                                                                            [-0.8,1.,0.5],
                                                                            [0.,0.5,1.]]):
@@ -32,20 +33,8 @@ def getdata(dof=5, samp=10000, mean=[0.,2.,0.], final_cov=[1.,2.,1.], cov=[[1.,-
     x1=m1.ppf(x_unif[:,1]).reshape(samp,1)
     x2=m2.ppf(x_unif[:,2]).reshape(samp,1)
     return np.concatenate((x0,x1,x2), axis=1)
-def getdata_2d(dof=5, samp=10000, mean=[0.,1.], final_cov=[1., 2.], cov=[[1.,-0.8], [-0.8,1.]]):
- 
-    mvnorm=st.multivariate_normal(mean=mean, cov=cov)
-    x=mvnorm.rvs(samp, random_state=7)
-    norm=st.norm()
-    x_unif=norm.cdf(x)
-    m0=st.norm(loc=mean[0], scale=final_cov[0])
-    m1=st.norm(loc=mean[1], scale=final_cov[1])
-  
-    x0=m0.ppf(x_unif[:,0]).reshape(samp,1)
-    x1=m1.ppf(x_unif[:,1]).reshape(samp,1)
-  
-    return np.concatenate((x0,x1), axis=1)
 
+#plot histogram of data and correlation heatmap  
 def plots(data, labels):
     axlabels=[]
     #histogram
@@ -68,9 +57,8 @@ def plots(data, labels):
     #plt.savefig("correlation "+ labels[0])
     #plt.close()
     
+#compute some summary statistics
 def statis(data):
-    
-    
     if data.ndim!=3:
         a=pd.DataFrame(columns=["Mean", "Std. Dev.", "1st perc", "99th perc"], index=np.arange(data.shape[1]))
         #if its only real data and not samples
@@ -91,6 +79,7 @@ def statis(data):
         a["99th perc Std"]=np.std(np.percentile(data, 0.99, axis=0), axis=-1)
     return a
 
+#QQ Plots of model generated data vs known theoretical distribution
 def QQ(data,dist, dof=5, mean=[0.,2.,0.], cov=[1.,2.,1.]):
   
     for i in range(data.shape[1]):
@@ -110,29 +99,21 @@ if __name__ == '__main__':
     scaler = MinMaxScaler(feature_range=(0.00001, 0.99999))
     data_scaled = scaler.fit_transform(data)
    
-
-    from vae_tf1 import CVAE
-    CVAE=CVAE(n_latent=2, n_hidden=100,alpha=0.5)
-    #CVAE.train(res_norm,n_epochs=1000, learning_rate=0.0001, show_progress=True)
+    #create and train VAE
+    VAE=VAE(n_latent=2, n_hidden=100,alpha=0.5)
+    VAE.train(data=data_scaled, n_epochs=1000, learning_rate=0.0001, show_progress=True)
     
-    
-
-    CVAE.train(data=data_scaled, n_epochs=1000, learning_rate=0.0001, show_progress=True)
-    
+    #%%
+    #robustness test with statistics calculation/plotting 
     path_len=5000
     num_path=20
     res=np.zeros((path_len, data.shape[1],num_path))
     for i in range(num_path):
         res_norm=CVAE.generate(5000)
-        res[:,:,i]=scaler.inverse_transform(res_norm)
-        
-        
+        res[:,:,i]=scaler.inverse_transform(res_norm)   
     stat_cvae=statis(res)
-    
     res.sort(axis=0)
     mean_res=np.mean(res, axis=-1)
-   
-    
     dist=["st.norm", "st.norm", "st.t"]
     QQ(mean_res, dist)
    
